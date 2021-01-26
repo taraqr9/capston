3a364a52f51ae748935c4d639cb0dcb70080fad1
taraqr9 <taraqr9@gmail.com>
2021-01-27 00:02:05 +0600

    test push

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
-include "../init.php";
-include "headerfile.php";
+include "init.php";
+include "admin/product/splitfile/headerfile.php";
 
 ?>
 <!-- Testing Purpuse -->
@@ -21,7 +21,7 @@ include "headerfile.php";
 
 <body>
   <!-- navbar -->
-  <?php include 'splitfile/navbar.php' ?>
+  <?php include 'admin/product/splitfile/navbar.php' ?>
 
   <div class="container text-success">
     <?php
@@ -42,66 +42,25 @@ include "headerfile.php";
         <thead>
           <tr>
 
-            <th class="col-1 border-right">Order Id</th>
             <th class="col-1 border-right">Product Id</th>
-            <th class="col-1 border-right">User Id</th>
-            <th class="col-1 border-right">QTY</th>
-            <th class="col-1 border-right">Size</th>
-            <th class="col-1 border-right">Price</th>
             <th class="col-1 border-right">Name</th>
-            <th class="col-1 border-right">Address</th>
-            <th class="col-1 border-right">Phone</th>
-            <th class="col-1 border-right">Email</th>
-            <th class="col-1 border-right">Approval</th>
+            <th class="col-1 border-right">QTY</th>
+            <th class="col-1 border-right">Price</th>
           </tr>
         </thead>
         <tbody>
           <?php
-          $query = $source->Query("SELECT * FROM `order` where status='Approved'");
+          $query = $source->Query("SELECT * FROM `products` where season = '2'");
           $details = $source->FetchAll();
           $numrow = $source->CountRows();
           if ($numrow > 0) {
             foreach ($details as $row) :
-              
-
-
-
               echo "
                 <tr>
-                
-                <td class='border-right'>" . $row->oid . "</td>
-                <td class='border-right'>" . $row->pid . "</td>
-                <td class='border-right'>" . $row->uid . "</td>
-                <td class='border-right'>" . $row->qty . "</td>";
-              if (!empty($row->size)) {
-                echo "<td>" . $row->size . "</td>";
-              } else {
-                echo "<td class='text-danger
-                  '>Null</td>";
-              }
-              echo "
-                <td class='border-right'>" . $row->price . "</td>
+                <td class='border-right'>" . $row->id . "</td>
                 <td class='border-right'>" . $row->name . "</td>
-                <td class='border-right'>" . $row->address . "</td>
-                <td class='border-right'>" . $row->phone . "</td>
-                <td class='border-right'>" . $row->email . "</td>
-                <td class='border-right' " . $approval_text . ">" . $row->status . "</td>
-                <td>" . "<a href='pendingsql.php?pending=" . $row->oid . "' class=' btn btn-outline-warning text-dark mt-2'> Pending</a>" . $app . " </td>";
-                
-                if (!empty($_SESSION['admin_log'])) {
-                  if ($_SESSION['admin_log'] == '1') {
-                    if ($row->status === 'Approved') {
-                      $app = "<a href='delete.php?deleteorder=" . $row->oid . "' class='btn btn-outline-danger mt-2'> Delete</a>";
-                      $approval_text = "class = text-info";
-                    } elseif ($row->status === 'Canceled') {
-                      $approval_text = "class = text-danger";
-                    } else {
-                      $app = "";
-                      $approval_text = "class = text-success text-uppercase";
-                    }
-                  }
-                }
-
+                <td class='border-right'>" . $row->qty . "</td>
+                <td class='border-right'>" . $row->price . "</td>";
                 echo"
                 </tr>";
 
