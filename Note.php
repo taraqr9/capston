c3fa3939dec94b93a86a6063321c83bb2641aab6
taraqr9 <taraqr9@gmail.com>
2021-01-29 01:16:17 +0600

    29/01/21

---
+++ Note.php
@@ -0,0 +1,5 @@
+<?php
+// NOTE Last month e kon product koyta sell hoice oita calculation korbona, coz ami proti month er hisabo oita month er 25 tarike korteci.
+// Jodi oi product ta 25 tarikher age ses hoy tahole dekhbe current date 20 ar soman or niche ki na. jodi soman or niche hoy tahole total sell ar 60% product anbe ar na hole 20% anbe.
+
+?>
--- test.php
+++ test.php
@@ -21,35 +21,23 @@ include "admin/product/splitfile/headerfile.php";
 
 <body>
   <!-- navbar -->
-  <?php include 'admin/product/splitfile/navbar.php' ?>
 
-  <div class="container text-success">
-    <?php
-    if (!empty($_SESSION['approve_user'])) {
-      echo $_SESSION['approve_user'];
-      $_SESSION['approve_user'] = "";
-    }
-    if (!empty($_SESSION['delete_order'])) {
-      echo $_SESSION['delete_order'];
-      $_SESSION['delete_order'] = "";
-    }
-    ?>
-  </div>
   <!-- View Pending Events -->
   <div class="container-fluid">
     <div class="container">
       <table class="table table-hover border">
         <thead>
           <tr>
-
             <th class="col-1 border-right">Product Id</th>
             <th class="col-1 border-right">Name</th>
             <th class="col-1 border-right">QTY</th>
+            <th class="col-1 border-right">NEED QTY</th>
             <th class="col-1 border-right">Price</th>
+            <th class="col-1 border-right">Category</th>
+            <th class="col-1 border-right">Sub Category</th>
           </tr>
         </thead>
-        <tbody>
-
+        
           <?php
           // NOTE Current season 
           $date = date('M');
@@ -59,58 +47,68 @@ include "admin/product/splitfile/headerfile.php";
           $currentSeason = $row->season;
           $currentMonth = $row->id;
 
-
-
-
-          $query = $source->Query("SELECT * FROM `products` where season = $currentSeason and qty <= '15'");
+          //NOTE Getting product from product table where qty lower than 15
+          $query = $source->Query("SELECT * FROM `products` where season = $currentSeason and qty <= '15' and day(current_date)<=30");
           $lowqty = $source->FetchAll();
           $numrow = $source->CountRows();
-          $allpid = [];
-          if ($numrow > 0) {
-
-
-
-
+          echo $numrow;
             foreach ($lowqty as $row) :
-              // NOTE Next season
+              echo "<tbody>";
+              // NOTE Next Month
               if ($currentMonth != `12`) {
                 $nextMonth  = $currentMonth + 1;
               } else {
                 $nextMonth = 1;
               }
+              // NOTE next season
               $mon = $source->Query("SELECT * FROM `month` where `id` = $nextMonth");
               $nextS = $source->SingleRow();
               $nextSeason = $nextS->season;
 
-              
+              $productid = $row->id;
+
               // NOTE CURRENT SEASON  == NEXT SEASON then product will come 60% of total order qty.
               // else 20% of the total order of the product id
-              if ($currentSeason == $nextSeason && !in_array($check, $allpid)) {
+              if ($currentSeason == $nextSeason) {
+                $query = $source->Query("SELECT pid,pname,sum(qty) as qtyy,price,category,sub_category  FROM `order` where pid = $row->id and month(date) = month(CURRENT_DATE) and year(date) = year(CURRENT_DATE)");
+                $featchRow = $source->SingleRow();
+
+                //NOTE 60% qty for product
+                $parcent = (int)(($featchRow->qtyy * 60) / 100);
+
+                echo "
+                <tr>
                 
-                // FIXME Product er qty sum kore print korte hobe
-                // FIXME jodi print hoy then oitake 60% korte hobe total sell qty er.
-                
-                // $query = $source->Query("SELECT pid,oid,uid,date,pname,sum(qty) as qtyy,price,category,sub_category  FROM `order` where pid = $row->id");
-                // $featchRow = $source->SingleRow();
-                
-                // echo "
-                // <tr>
-                // <td class='border-right'>" . $featchRow->pid . "</td>
-                // <td class='border-right'>" . $featchRow->pname . "</td>
-                // <td class='border-right'>" . $featchRow->qqty . "</td>
-                // <td class='border-right'>" . $featchRow->oid . "</td>";
-                // echo "
-                // </tr>";
+                <td class='border-right'>" . $featchRow->pid . "</td>
+                <td class='border-right'>" . $featchRow->pname . "</td>
+                <td class='border-right'>" . $featchRow->qtyy . "</td>
+                <td class='border-right'>" . $parcent . "</td>
+                <td class='border-right'>" . $featchRow->price . "</td>
+                <td class='border-right'>" . $featchRow->category . "</td>
+                <td class='border-right'>" . $featchRow->sub_category . "</td>";
+                echo "
+                </tr>";
               } else {
+                $query = $source->Query("SELECT pid,pname,sum(qty) as qtyy,price,category,sub_category  FROM `order` where pid = $row->id and month(date) = month(CURRENT_DATE) and year(date) = year(CURRENT_DATE)");
+                $featchRow = $source->SingleRow();
+                //NOTE 20% qty for product
+                $parcent = (int)(($featchRow->qtyy * 20) / 100);
+                echo "
+                <tr>
+                <td class='border-right'>" . $featchRow->pid . "</td>
+                <td class='border-right'>" . $featchRow->pname . "</td>
+                <td class='border-right'>" . $featchRow->qtyy . "</td>
+                <td class='border-right'>" . $parcent . "</td>
+                <td class='border-right'>" . $featchRow->price . "</td>
+                <td class='border-right'>" . $featchRow->category . "</td>
+                <td class='border-right'>" . $featchRow->sub_category . "</td>";
+                echo "
+                </tr></tbody>";
               }
-
-            $allpid[] = $check;
-
+              echo $row->id."<br>";
             endforeach;
-          }
-
           ?>
-        </tbody>
+        
       </table>
     </div>
   </div>
